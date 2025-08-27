dabec4dfd6d441d7c0d2dedea7f485dde4a13206
Raji G <rajith0417@gmail.com>
2025-08-27 13:55:04 +0530

    fix: trying to fix image issue

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,11 @@
 import type { NextConfig } from "next";
 
+const isProd = process.env.NODE_ENV === 'production'
+
 const nextConfig: NextConfig = {
   output: 'export', // 👈 enables static export
-  basePath: '/escapeelite',
-  assetPrefix: '/escapeelite/',
+  basePath: isProd ? '/escapeelite' : '',
+  assetPrefix: isProd ? '/escapeelite/' : '',
   images: {
     unoptimized: true,           // required for static export
   },
--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -4,7 +4,7 @@ export default function Hero() {
   return (
     <section
       id="home"
-      className="h-screen bg-cover bg-center flex flex-row items-center text-white text-center bg-[url(/banners/image9.png)]"    >
+      className="h-screen bg-cover bg-center flex flex-row items-center text-white text-center bg-[url(./banners/image9.png)]"    >
       <div className="absolute z-0 inset-0 bg-black opacity-50"></div>
       <div className="hidden container float-left z-10 w-1/2 md:block pl-16">
         <h2 className="text-4xl md:text-4xl font-medium mb-4 text-left">
