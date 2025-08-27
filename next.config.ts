0fe73a2866c9fcc7c38316791ba11d115889ef5c
Raji G <rajith0417@gmail.com>
2025-08-27 08:19:15 +0530

    fix: modified to fix 404 error

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,9 @@ const nextConfig: NextConfig = {
   output: 'export', // 👈 enables static export
   basePath: '/escapeelite',
   assetPrefix: '/escapeelite/',
+  images: {
+    unoptimized: true,           // required for static export
+  },
 };
 
 export default nextConfig;
