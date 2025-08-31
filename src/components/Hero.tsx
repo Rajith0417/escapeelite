0891f0e999d6f467fa766bac337f4b7f48f5b43a
Raji G <rajith0417@gmail.com>
2025-08-31 19:05:48 +0530

    fix: hero alignment issue fixed

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,30 +1,25 @@
 import React from "react";
-import ImageGallery from "./ImageGallery";
 import Image from "next/image";
 
 export default function Hero() {
   return (
     <section
       id="home"
-      className="h-screen flex flex-col md:flex-row items-center text-white text-center]"
+      className="relative h-screen"
     >
       <Image
         src="banners/image9.png"
         alt=""
-        width={0}
-        height={0}
-        className="absolute w-full h-full object-fill"
+        fill
+        className="object-cover object-center"
       />
       <div className="absolute z-0 inset-0 bg-black opacity-50"></div>
-      <div className="container px-5 mx-auto flex flex-col md:flex-row">
-        <div className="hidden float-left z-10 w-full md:w-1/2 md:block pl-16">
+      <div className="container h-full w-full px-5 mx-auto flex flex-col md:flex-row items-center justify-center md:justify-start text-white text-center]">
+        <div className="hidden float-left z-10 md:w-1/2 md:block pl-16">
           <h2 className="text-4xl md:text-4xl font-medium mb-4 text-left">
             We Specialize in Sri Lanka & the Maldives – Get Your Free Custom
             Itinerary!
           </h2>
-        </div>
-        <div className="w-full md:w-1/2">
-          <ImageGallery />
         </div>
         <div className="px-16 z-10 md:hidden">
           <h2 className="text-4xl md:text-4xl font-medium mb-4 text-center">
