6298d6ed6b12073bb1b125ac8c8839f9ec3052bc
Raji G <rajith0417@gmail.com>
2025-09-02 23:23:10 +0530

    fix: hero updated for homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,5 @@
 "use client";
-import Attractions from "@/components/Attraction";
-import FeaturedAccommodationSection from "@/components/FeaturedAccommodationSection";
+import AttractionsSection from "@/components/AttractionSection";
 import FeaturedHolidaysSection from "@/components/FeaturedHolidaysSection";
 import FeaturedHolidaysDetails from "@/components/FeaturedHolidaysDetails";
 import Hero from "@/components/Hero";
@@ -15,20 +14,24 @@ import ChatbotPage from "@/components/ChatbotPage";
 export default function HomePage() {
   return (
     <>
-      <Hero />
+      <Hero
+        image="banners/image9.png"
+        titleDesktop="We Specialize in Sri Lanka & the Maldives – Get Your Free Custom
+            Itinerary!"
+        titleMobile="Welcome to Escape Elite!"
+      />
       <InfoSection />
-      <FeaturedHolidaysSection/>
-      <TestimonialSection/>
-      <MoreDetails/>
-      <Attractions/>
+      <FeaturedHolidaysSection />
+      <TestimonialSection />
+      <MoreDetails />
+      <AttractionsSection />
       <FeaturedHolidaysDetails
         heading="Featured holidays to MALDIVES"
         dropdownOptions={["All Holidays", "Honeymoon", "Family", "Luxury"]}
         selectedOption="All Holidays"
         onDropdownChange={(val) => console.log(val)}
       />
-      <FeaturedAccommodationSection />
-      <ItinerarySection/>
+      <ItinerarySection />
       <section>
         <div className="container mx-auto px-4 py-12">
           <div className="grid grid-cols-1 md:grid-cols-2 gap-8 items-center">
@@ -36,7 +39,7 @@ export default function HomePage() {
               <ImageGallery />
             </div>
             <div>
-              <ChatbotPage questionnaireId="b91db0d7-e9b2-4432-bc19-0c90f894f407" />
+              {/* <ChatbotPage questionnaireId="b91db0d7-e9b2-4432-bc19-0c90f894f407" /> */}
             </div>
           </div>
         </div>
