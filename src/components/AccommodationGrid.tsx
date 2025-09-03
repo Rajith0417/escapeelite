2afb113f195c537693d4c54897fca9517091f966
Raji G <rajith0417@gmail.com>
2025-09-03 18:40:37 +0530

    fix: correct url added

--- src/components/AccommodationGrid.tsx
+++ src/components/AccommodationGrid.tsx
@@ -39,7 +39,7 @@ export default function AccommodationGrid({
           description={accommodation.description}
           rating={accommodation.rating}
           tag={accommodation.tag}
-          onViewDetailsHref={`/accommodation/${accommodation.id}`}
+          onViewDetailsHref={`/hotels/${accommodation.id}`}
         />
       ))}
     </div>
